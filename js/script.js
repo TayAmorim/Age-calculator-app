24849dcde29e66b5ee90aca921bfefc18fd3e442
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-08 11:09:05 -0300

    pegando os valores do input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-const inputs = document.querySelectorAll("[data-input]");
+const inputDay = document.querySelector("[data-input='day']");
+const inputMonth = document.querySelector("[data-input='month']");
+const inputYear = document.querySelector("[data-input='year']");
 const btn = document.querySelector(".btn-img");
 
-console.log(btn);
+let day = 0;
+let month = 0;
+let year = 0;
+
+btn.addEventListener("click", calculateAge);
+const dataInput = [];
+
+function calculateAge() {
+  day = +inputDay.value;
+  month = +inputMonth.value;
+  year = +inputYear.value;
+}
+console.log(day, month, year);
