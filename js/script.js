167d9d2db1f6ac4c88043ae3fca35f99eaa875e6
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-15 11:33:36 -0300

    continuando a validação do campo vazio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-const inputDay = document.querySelector("[data-input='day']");
-const inputMonth = document.querySelector("[data-input='month']");
-const inputYear = document.querySelector("[data-input='year']");
+const inputs = document.querySelectorAll("[data-input]");
+
 const btn = document.querySelector(".btn-img");
+const resultYears = document.querySelector('[data-result="year"]');
+const resultMonth = document.querySelector('[data-result="month"]');
+const resulDays = document.querySelector('[data-result="days"]');
+const cardsDate = document.querySelectorAll(".card-date");
 
 let day = 0;
 let month = 0;
 let year = 0;
+let visibleMessage = false;
 
-btn.addEventListener("click", calculateAge);
-const dataInput = [];
+btn.addEventListener("click", validateinputs);
 
-function calculateAge() {
-  day = +inputDay.value;
-  month = +inputMonth.value;
-  year = +inputYear.value;
+function validateinputs() {
+  inputs.forEach((input) => {
+    if (input.value === "" && !visibleMessage) {
+      const span = document.createElement("span");
+      input.after(span);
+      span.innerText = "Esse campo é requerido.";
+      span.className = `${input.dataset.input}`;
+    }
+    if (input.value !== "" && visibleMessage) {
+      const elementDelet = document.querySelector(`.${input.dataset.input}`);
+      elementDelet.innerHTML = "";
+    }
+    console.log(visibleMessage);
+  });
+  visibleMessage = true;
 }
-console.log(day, month, year);
+
+/* function calculateAge() {
+  const age = new Date().getFullYear() - year;
+  resultYears.innerText = age;
+} */
