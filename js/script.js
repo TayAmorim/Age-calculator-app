18639ca95c73a836793af30ac07e46dfee3f88fd
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-17 18:10:23 -0300

    validação dia de acordo com o mês[

--- js/script.js
+++ js/script.js
@@ -6,19 +6,23 @@ const resultMonth = document.querySelector('[data-result="month"]');
 const resulDays = document.querySelector('[data-result="days"]');
 const cardsDateText = document.querySelectorAll(".card-date-text");
 
-const valueImput = [];
 let day = 0;
 let month = 0;
 let year = 0;
+
+const months = [undefined, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const visibleMessage = {
   day: false,
   month: false,
   year: false,
 };
-btn.addEventListener("click", validateinputs);
+btn.addEventListener("click", validateEmptyField);
 
-function validateinputs() {
+function validateEmptyField() {
+  day = 0;
+  month = 0;
+  year = 0;
   for (let i = 0; i < inputs.length; i++) {
     const input = inputs[i];
     const inputValue = input.value;
@@ -42,12 +46,35 @@ function validateinputs() {
       visibleMessage[`${nameInput}`] = false;
     }
     if (inputValue) {
-      valueImput.push(inputValue);
+      if (input.dataset.input === "day") {
+        day = inputValue;
+      }
+      if (input.dataset.input === "month") {
+        month = inputValue;
+      }
+      if (input.dataset.input === "year") {
+        year = inputValue;
+      }
     }
   }
-  day = valueImput[0];
-  month = valueImput[1];
-  year = valueImput[2];
+  if (day && month && year) {
+    validateDate();
+  }
+}
+
+function validateDate() {
+  if (month >= 1 && month <= 9) {
+    let monthClear = month.toString().replace("0", "");
+    const monthDay = months[`${+monthClear}`];
+    if (day > monthDay) {
+      console.log("data errada");
+    }
+  } else if (month >= 10 && month <= 12) {
+    const monthDay = months[`${+month}`];
+    if (day > monthDay) {
+      console.log("data errada");
+    }
+  }
 }
 
 /* function calculateAge() {
